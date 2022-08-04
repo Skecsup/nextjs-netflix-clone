143cda33d3e355810e821cadfe2609b5acafff33
Skecsup <kicsindy.julius@gmail.com>
2022-08-04 14:30:24 +0200

    plans page done, need to do redirection

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -2,15 +2,27 @@ import { CheckIcon } from "@heroicons/react/outline";
 import { Product } from "@stripe/firestore-stripe-payments";
 import Head from "next/head";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import useAuth from "../hooks/useAuth";
+import { loadCheckout } from "../lib/stripe";
+import Loader from "./Loader";
+import Table from "./Table";
 
 interface Props {
   products: Product[];
 }
 
 const Plans = ({ products }: Props) => {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
+  const [selectedPlan, setSelectedPlan] = useState<Product | null>(products[2]);
+  const [isBillingLoading, setBillingLoading] = useState(false);
+
+  const subscribeToPlan = () => {
+    if (!user) return;
+    loadCheckout(selectedPlan?.prices[0].id!);
+    setBillingLoading(true);
+  };
+
   return (
     <div>
       <Head>
@@ -34,7 +46,7 @@ const Plans = ({ products }: Props) => {
           Sign Out
         </button>
       </header>
-      <main className="max-w-5xl px-5 pt-28 pb-12 transition-all md:px-10">
+      <main className="mx-auto max-w-5xl px-5 pt-28 pb-12 transition-all md:px-10">
         <h1 className="mb-3 text-3xl font-medium">
           Choose the plan that's right for you
         </h1>
@@ -55,13 +67,31 @@ const Plans = ({ products }: Props) => {
         <div className="mt-4 flex flex-col space-y-4">
           <div className="flex w-full items-center justify-end self-end md:w-3/5">
             {products.map((product) => (
-              <div className="planBox" key={product.id}>
+              <div
+                className={`planBox ${
+                  selectedPlan?.id === product.id ? "opacity-100" : "opacity-60"
+                }`}
+                onClick={() => setSelectedPlan(product)}
+                key={product.id}
+              >
                 {product.name}
               </div>
             ))}
           </div>
-          {/* <Table/> */}
-          <button>Subscribe</button>
+          <Table selectedPlan={selectedPlan} products={products} />
+          <button
+            disabled={!selectedPlan || isBillingLoading}
+            className={`mx-auto w-11/12 rounded bg-[#E50914] py-4 text-xl shadow hover:bg-[#f6121d] md:w-[420px] ${
+              isBillingLoading && "opacity-60"
+            }`}
+            onClick={subscribeToPlan}
+          >
+            {isBillingLoading ? (
+              <Loader color="dark:fill-gray-300" />
+            ) : (
+              "Subscribe"
+            )}
+          </button>
         </div>
       </main>
     </div>
